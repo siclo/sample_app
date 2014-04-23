7dabde272f1eeb4ac3bdf90c2efc29f09a50d4ae
Simon Cloutier <simon.cloutier@gmail.com>
2014-04-22 22:06:12 -0400

    Add failing test for the About page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  describe "Static pages" do
+  describe "Home page" do
 
     it "should have the content 'Sample App'" do
 	    visit '/static_pages/home'
@@ -15,6 +15,14 @@ describe "StaticPages" do
 	  it "should have the content 'Help'" do
 		  visit '/static_pages/help'
 		  expect(page).to have_content('Help')
+	  end
+  end
+
+  describe "About page" do
+
+	  it "should have the content 'About Us'" do
+		  visit '/static_pages/about'
+		  expect(page).to have_content('About Us')
 	  end
   end
 end
