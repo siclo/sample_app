da0c46a0efffee70aba0f913aab3e17336cd4fcc
Simon Cloutier <simon.cloutier@gmail.com>
2014-07-01 22:10:50 -0400

    Added spork to speed up tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -41,4 +51,11 @@ RSpec.configure do |config|
   config.order = "random"
 
   config.include Capybara::DSL
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
+
 end
